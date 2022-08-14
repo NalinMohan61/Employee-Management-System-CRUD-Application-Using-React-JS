9c36d9e2e9de041242b90d422b93c74934955c3f
NalinMohan61 <106859940+NalinMohan61@users.noreply.github.com>
2022-08-14 16:55:55 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,6 @@
+import EmployeeDetails from "./EmployeeDetails";
+import EmployeeTable from "./EmployeeTable";
+import FormPopup from "./FormPopup";
+import RemovePopup from "./RemovePopup";
+
+export { EmployeeDetails, EmployeeTable, FormPopup, RemovePopup };
